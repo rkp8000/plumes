b99a4ecb6f4c14ef0c3cb59169e57d6dabbe4a52
rkp8000 <rpang.contact@gmail.com>
2015-06-05 15:10:00 -0700

    Clean up unit tests so they do what I want.

--- test.py
+++ test.py
@@ -99,9 +99,34 @@ class DiscretizationTestCase(unittest.TestCase):
         xrbins = np.linspace(0, 1., 11)
         yrbins = np.linspace(0, 1., 11)
         zrbins = np.linspace(0, 1., 11)
-        self.env = Environment3d(xrbins, yrbins, zrbins)
+        self.env = plume.Environment3d(xrbins, yrbins, zrbins)
 
-    def test_straight_line_trajectory_discretization_by_trial_instance(self):
+    def test_diagonalest_lattice_path(self):
+        starts_and_ends = (((0, 0, 0), (4, 0, 0)),
+                           ((0, 0, 0), (1, 1, 1)),
+                           ((0, 0, 0), (2, 2, 2)),
+                           ((1, 2, 3), (0, 0, 1)))
+
+        true_path_lengths = (4, 3, 6, 5)
+
+        # loop through every start & end pair and check to make sure the generated path is correct
+        for start_and_end, true_path_length in zip(starts_and_ends, true_path_lengths):
+            start, end = start_and_end
+            pos_idxs = self.env.diagonalest_lattice_path(start, end)
+
+            # check path length
+            self.assertEqual(len(pos_idxs), true_path_length)
+            # check that first element is one different from start
+            self.assertEqual(np.abs(np.array(start) - pos_idxs[0]).sum(), 1)
+            # check that last element is end
+            self.assertTrue(np.all(np.array(end) == pos_idxs[-1]))
+
+            # check that each pos idx is one step away from the previous pos idx
+            for pi_ctr, pos_idx in enumerate(pos_idxs[:-1]):
+                next_pos_idx = np.array(pos_idxs[pi_ctr + 1])
+                self.assertEqual(np.abs(pos_idx - next_pos_idx).sum(), 1)
+
+    def test_straight_line_trajectory_discretization_by_environment3d(self):
 
         # this trajectory should have 11 timesteps when mapped onto the grid in env
         x = 0.55 * np.ones((30,))
@@ -112,9 +137,9 @@ class DiscretizationTestCase(unittest.TestCase):
         pos_idxs = self.env.discretize_position_sequence(positions)
 
         # check to make sure duration is correct
-        self.assertEqual(len(idxs), 10)
+        self.assertEqual(len(pos_idxs), 10)
 
-    def test_perfectly_diagonal_trajectory_discretization_by_trial_instance(self):
+    def test_perfectly_diagonal_trajectory_discretization_by_environment3d(self):
         x = np.linspace(.15, .75, 40)
         y = np.linspace(.15, .75, 40)
         z = np.linspace(.15, .75, 40)
@@ -124,21 +149,21 @@ class DiscretizationTestCase(unittest.TestCase):
 
         # check to make sure duration is correct
         true_duration = 19
-        self.assertEqual(len(idxs), true_duration)
+        self.assertEqual(len(pos_idxs), true_duration)
 
         # check that each pos idx is one step away from the previous pos idx
         for pi_ctr, pos_idx in enumerate(pos_idxs[:-1]):
-            next_pos_idx = np.array(trial.pos_idx[pi_ctr + 1])
+            next_pos_idx = np.array(pos_idxs[pi_ctr + 1])
             self.assertEqual(np.abs(pos_idx - next_pos_idx).sum(), 1)
 
-    def test_random_walk_trajectory_discretization_by_trial_instance(self):
+    def test_random_walk_trajectory_discretization_by_environment3d(self):
 
         # loop over some more random trajectories
         for _ in range(5):
 
-            x = 0.5 + np.random.normal(0, .003, (1000,)).sum()
-            y = 0.5 + np.random.normal(0, .003, (1000,)).sum()
-            z = 0.5 + np.random.normal(0, .003, (1000,)).sum()
+            x = 0.5 + np.random.normal(0, .003, (1000,)).cumsum()
+            y = 0.5 + np.random.normal(0, .003, (1000,)).cumsum()
+            z = 0.5 + np.random.normal(0, .003, (1000,)).cumsum()
             positions = np.array([x, y, z]).T
             # truncate positions if any of them go beyond 1 or 0
             outside_env = [ts for ts, pos in enumerate(positions) if np.any(pos > 1) or np.any(pos < 0)]
@@ -149,12 +174,12 @@ class DiscretizationTestCase(unittest.TestCase):
 
             # check that each pos idx is one step away from the previous pos idx
             for pi_ctr, pos_idx in enumerate(pos_idxs[:-1]):
-                next_pos_idx = np.array(trial.pos_idx[pi_ctr + 1])
+                next_pos_idx = np.array(pos_idxs[pi_ctr + 1])
                 self.assertEqual(np.abs(pos_idx - next_pos_idx).sum(), 1)
 
             # check that first and last pos idx are what env would give them
-            first_pos_idx_env = np.array(self.env.idx_from_pos[positions[0]])
-            last_pos_idx_env = np.array(self.env.idx_from_pos[positions[-1]])
+            first_pos_idx_env = np.array(self.env.idx_from_pos(positions[0]))
+            last_pos_idx_env = np.array(self.env.idx_from_pos(positions[-1]))
             np.testing.assert_array_equal(first_pos_idx_env, np.array(pos_idxs[0]))
             np.testing.assert_array_equal(last_pos_idx_env, np.array(pos_idxs[-1]))
 
